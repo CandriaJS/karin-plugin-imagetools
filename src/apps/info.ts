06ee9b5173197e5a2cd7533aea7edb89b530e0ba
wuliya <3369906077@qq.com>
2025-10-11 18:06:30 +0800

    refactor(utils): 重构图片处理与文件发送功能
    -优化 get_image 函数泛型支持，增强类型安全
    - 调整图片信息类型定义，支持 buffer 模式判断

--- src/apps/info.ts
+++ src/apps/info.ts
@@ -1,10 +1,10 @@
-import * as imageTool from '@candriajs/image-tool'
+import { getImageInfo } from '@puniyu-plugins/piccy'
 import karin, { logger, Message, segment } from 'node-karin'
 
 import { utils } from '@/models'
 import { Version } from '@/root'
 
-export const info = karin.command(/^#?(?:(?:柠糖)(?:图片操作|imagetools))?(?:查看)?(?:图片信息|imageinfo)$/i, async (e: Message) => {
+export const info = karin.command(/^#?(?:柠糖(?:图片操作|imagetools))?(?:查看)?(?:图片信息|imageinfo)$/i, async (e: Message) => {
   try {
     const getType = 'buffer'
     const image = await utils.get_image(e, getType)
@@ -13,16 +13,16 @@ export const info = karin.command(/^#?(?:(?:柠糖)(?:图片操作|imagetools))?
     if (!image_buffer) {
       return await e.reply('请发送图片', { reply: true })
     }
-    const image_info = imageTool.image_info(image_buffer as Buffer)
+    const image_info = getImageInfo(image_buffer)
     const replyMessage = [
       segment.image(`base64://${image_buffer.toString('base64')}`),
       segment.text('图片信息:\n'),
       segment.text(`分辨率: ${image_info.width}x${image_info.height}\n`),
-      segment.text(`是否为动图: ${image_info.is_multi_frame}\n`)
+      segment.text(`是否为动图: ${image_info.isMultiFrame}\n`)
     ]
-    if (image_info.is_multi_frame) {
-      replyMessage.push(segment.text(`帧数: ${image_info.frame_count}\n`))
-      replyMessage.push(segment.text(`动图平均帧率: ${image_info.average_duration}\n`))
+    if (image_info.isMultiFrame) {
+      replyMessage.push(segment.text(`帧数: ${image_info.frameCount}\n`))
+      replyMessage.push(segment.text(`动图平均帧率: ${image_info.averageDuration}\n`))
     }
     await e.reply(replyMessage, { reply: true })
   } catch (error) {
--- src/models/utils/common.ts
+++ src/models/utils/common.ts
@@ -1,6 +1,6 @@
-import karin, { base64, buffer, Elements, ImageElement, Message, MessageResponse } from 'node-karin'
+import karin, {base64, buffer, Message, MessageResponse} from 'node-karin'
 
-import { ImageInfoType } from '@/types'
+import {ImageInfoType} from '@/types'
 
 import Request from './request'
 
@@ -10,29 +10,29 @@ import Request from './request'
  * @throws 如果获取 IP 位置失败，则抛出异常
  */
 export const isAbroad = async (): Promise<boolean> => {
-  const urls = [
-    'https://blog.cloudflare.com/cdn-cgi/trace',
-    'https://developers.cloudflare.com/cdn-cgi/trace',
-    'https://hostinger.com/cdn-cgi/trace',
-    'https://ahrefs.com/cdn-cgi/trace'
-  ]
+    const urls = [
+        'https://blog.cloudflare.com/cdn-cgi/trace',
+        'https://developers.cloudflare.com/cdn-cgi/trace',
+        'https://hostinger.com/cdn-cgi/trace',
+        'https://ahrefs.com/cdn-cgi/trace'
+    ]
 
-  try {
-    const responses = await Promise.all(
-      urls.map((url) => Request.get(url, null, null, 'text'))
-    )
-    const traceTexts = responses.map((res) => res.data).filter(Boolean)
-    const traceLines = traceTexts
-      .flatMap((text: string) =>
-        text.split('\n').filter((line: string) => line)
-      )
-      .map((line) => line.split('='))
+    try {
+        const responses = await Promise.all(
+            urls.map((url) => Request.get(url, null, null, 'text'))
+        )
+        const traceTexts = responses.map((res) => res.data).filter(Boolean)
+        const traceLines = traceTexts
+            .flatMap((text: string) =>
+                text.split('\n').filter((line: string) => line)
+            )
+            .map((line) => line.split('='))
 
-    const traceMap = Object.fromEntries(traceLines)
-    return traceMap.loc !== 'CN'
-  } catch (error) {
-    throw new Error(`获取 IP 所在地区出错: ${(error as Error).message}`)
-  }
+        const traceMap = Object.fromEntries(traceLines)
+        return traceMap.loc !== 'CN'
+    } catch (error) {
+        throw new Error(`获取 IP 所在地区出错: ${(error as Error).message}`)
+    }
 }
 
 /**
@@ -41,105 +41,103 @@ export const isAbroad = async (): Promise<boolean> => {
  * @param type 返回类型 url 或 base64
  * @returns 图片数组信息
  */
-export async function get_image (
-  e: Message,
-  type: 'url' | 'base64' | 'buffer' = 'url'
-): Promise<ImageInfoType[]> {
-  const imagesInMessage = e.elements
-    .filter((m) => m.type === 'image')
-    .map((img) => ({
-      userId: e.sender.userId,
-      image: img.file
-    }))
+export async function get_image<T extends 'url' | 'base64' | 'buffer' = 'url'>(
+    e: Message,
+    type: T = 'url' as T
+): Promise<Array<ImageInfoType<T>>> {
+    const imagesInMessage = e.elements
+        .filter((m) => m.type === 'image')
+        .map((img) => ({
+            userId: e.sender.userId,
+            image: img.file
+        }))
 
-  const tasks: Promise<ImageInfoType>[] = []
+    const tasks: Promise<ImageInfoType<T>>[] = []
 
-  let quotedImages: Array<ImageInfoType> = []
-  let source: MessageResponse | null = null
-  /**
-   * 获取引用消息的内容
-   */
-  const replyId: string | null = e.replyId ?? e.elements.find((m) => m.type === 'reply')?.messageId ?? null
+    let quotedImages: Array<ImageInfoType<T>> = []
+    let source: MessageResponse | null = null
+    /**
+     * 获取引用消息的内容
+     */
+    const replyId: string | null = e.replyId ?? e.elements.find((m) => m.type === 'reply')?.messageId ?? null
 
-  if (replyId) {
-    source = (await e.bot.getMsg(e.contact, replyId)) ?? null
-  }
+    if (replyId) {
+        source = (await e.bot.getMsg(e.contact, replyId)) ?? null
+    }
 
-  /**
-   * 提取引用消息中的图片
-   */
-  if (source) {
-    quotedImages = source.elements
-      .filter((m) => m.type === 'image')
-      .map((img) => ({
-        userId: source.sender.userId,
-        image: img.file
-      }))
-  }
+    /**
+     * 提取引用消息中的图片
+     */
+    if (source) {
+        quotedImages = source.elements
+            .filter((m) => m.type === 'image')
+            .map((img) => ({
+                userId: source.sender.userId,
+                image: img.file
+            })) as Array<ImageInfoType<T>>
+    }
 
-  /**
-   * 处理引用消息中的图片
-   */
-  if (quotedImages.length > 0) {
-    const quotedImagesPromises = quotedImages.map(async (item) => {
-      switch (type) {
-        case 'buffer':
-          return {
-            userId: item.userId,
-            image: await buffer(item.image)
-          }
-        case 'base64':
-          return {
-            userId: item.userId,
-            image: await base64(item.image)
-          }
-        case 'url':
-        default:
-          return {
-            userId: item.userId,
-            image: item.image.toString()
-          }
-      }
-    })
-    tasks.push(...quotedImagesPromises)
-  }
+    /**
+     * 处理引用消息中的图片
+     */
+    if (quotedImages.length > 0) {
+        const quotedImagesPromises = quotedImages.map(async (item) => {
+            switch (type) {
+                case 'buffer':
+                    return {
+                        userId: item.userId,
+                        image: await buffer(item.image)
+                    } as ImageInfoType<T>
+                case 'base64':
+                    return {
+                        userId: item.userId,
+                        image: await base64(item.image)
+                    } as ImageInfoType<T>
+                case 'url':
+                default:
+                    return {
+                        userId: item.userId,
+                        image: item.image.toString()
+                    } as ImageInfoType<T>
+            }
+        })
+        tasks.push(...quotedImagesPromises)
+    }
 
-  /**
-   * 处理消息中的图片
-   */
-  if (imagesInMessage.length > 0) {
-    const imagePromises = imagesInMessage.map(async (item) => {
-      switch (type) {
-        case 'buffer':
-          return {
-            userId: item.userId,
-            image: await buffer(item.image)
-          }
-        case 'base64':
-          return {
-            userId: item.userId,
-            image: await base64(item.image)
-          }
-        case 'url':
-        default:
-          return {
-            userId: item.userId,
-            image: item.image.toString()
-          }
-      }
-    })
-    tasks.push(...imagePromises)
-  }
+    /**
+     * 处理消息中的图片
+     */
+    if (imagesInMessage.length > 0) {
+        const imagePromises = imagesInMessage.map(async (item) => {
+            switch (type) {
+                case 'buffer':
+                    return {
+                        userId: item.userId,
+                        image: await buffer(item.image)
+                    } as ImageInfoType<T>
+                case 'base64':
+                    return {
+                        userId: item.userId,
+                        image: await base64(item.image)
+                    } as ImageInfoType<T>
+                case 'url':
+                default:
+                    return {
+                        userId: item.userId,
+                        image: item.image.toString()
+                    } as ImageInfoType<T>
+            }
+        })
+        tasks.push(...imagePromises)
+    }
 
-  const results = await Promise.allSettled(tasks)
-  const images = results
-    .filter(
-      (res): res is PromiseFulfilledResult<ImageInfoType> =>
-        res.status === 'fulfilled' && Boolean(res.value)
-    )
-    .map((res) => res.value)
-
-  return images
+    const results = await Promise.allSettled(tasks)
+    return results
+        .filter(
+            (res): res is PromiseFulfilledResult<ImageInfoType<T>> =>
+                res.status === 'fulfilled' && Boolean(res.value)
+        )
+        .map((res) => res.value)
 }
 
 /**
@@ -153,19 +151,19 @@ export async function get_image (
  * @param name 文件名称
  * @returns 发送结果
  */
-export async function send_file (type: 'group' | 'private', botId: number, id: number, file: string, name: string) {
-  try {
-    const bot = karin.getBot(String(botId))
-    let Contact
-    if (type === 'group') {
-      Contact = karin.contactGroup(String(id))
-    } else if (type === 'private') {
-      Contact = karin.contactFriend(String(id))
-    } else {
-      throw new Error('type 必须为 group 或 private')
+export async function send_file(type: 'group' | 'private', botId: number, id: number, file: string, name: string) {
+    try {
+        const bot = karin.getBot(String(botId))
+        let Contact
+        if (type === 'group') {
+            Contact = karin.contactGroup(String(id))
+        } else if (type === 'private') {
+            Contact = karin.contactFriend(String(id))
+        } else {
+            throw new Error('type 必须为 group 或 private')
+        }
+        return await bot?.uploadFile(Contact, file, name)
+    } catch (error) {
+        throw new Error(`向${type === 'group' ? '群' : '好友'} ${id} 发送文件失败: ${(error as Error).message}`)
     }
-    return await bot?.uploadFile(Contact, file, name)
-  } catch (error) {
-    throw new Error(`向${type === 'group' ? '群' : '好友'} ${id} 发送文件失败: ${(error as Error).message}`)
-  }
 }
